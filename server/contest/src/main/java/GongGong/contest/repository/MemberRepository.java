4193b97f7fc5ff30602c9af6a8c7935c3b50e737
DongUk <az9170@naver.com>
2024-06-26 16:11:50 +0900

    clear git cache

---
+++ server/contest/src/main/java/GongGong/contest/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package GongGong.contest.repository;
+
+import GongGong.contest.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+    
+}
---
+++ server/contest/src/main/java/GongGong/contest/service/MemberService.java
@@ -0,0 +1,26 @@
+package GongGong.contest.service;
+
+
+import GongGong.contest.domain.Member;
+import GongGong.contest.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    
+    private final MemberRepository memberRepository;
+    
+    public boolean joinMember(Member member) {
+        
+        /**
+         * 회원가입 로직 작성
+         * 중복 id 있는지 검사해서 true false 반환
+         */
+        
+        return true;
+    }
+    
+    
+}
