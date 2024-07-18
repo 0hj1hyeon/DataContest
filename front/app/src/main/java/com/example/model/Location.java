5529395625e8d1bb1aa59e0862fb85c372882113
DongUk <az9170@naver.com>
2024-07-18 14:21:53 +0900

    [app] 장소 model 추가

---
+++ front/app/src/main/java/com/example/model/Location.java
@@ -0,0 +1,36 @@
+package com.example.model;
+
+public class Location {
+
+    Double latitude;
+
+    Double longitude;
+    
+    String address;
+    
+    String name;
+    
+    String recruitment_period;
+    
+    String job_category;
+    
+    String employment_type;
+    
+    String salary_type;
+    
+    Integer salary;
+    
+    String entry_type;
+    
+    String required_experience;
+    
+    String required_education;
+    
+    String major_field;
+    
+    String certifications;
+    
+    String responsible_agency;
+    
+    String call_info;
+}
